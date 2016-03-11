f30cac998e401e92217810ec09a13bdbf5e42c7b
espa77 <ethansteiner@gmail.com>
2016-03-10 19:20:32 -0500

    added deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,11 @@
 lock '3.4.0'
 
+server '54.165.195.83', user: 'deploy', roles: %w{web app db}
+
 set :application, 'ethansteiner'
-set :repo_url, 'git@github.com:devdatta/contactbook.git' # Edit this to match your repository
+set :repo_url, 'git@github.com:espa77/portfolio_blog.git' # Edit this to match your repository
 set :branch, :master
-set :deploy_to, '/home/deploy/contactbook'
+set :deploy_to, '/home/deploy/ethansteiner'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
