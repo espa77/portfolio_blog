14de41cf6dde98f1521f9c4b86a38cfbaffd5bdc
espa77 <ethansteiner@gmail.com>
2016-05-16 15:35:32 -0400

    changed scraper for blog posts

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
 		agent = Mechanize.new { |agent| agent.user_agent_alias = "Mac Safari" }
 		html = agent.get(url).body
 		html_doc = Nokogiri::HTML(html)
-		@post_one = html_doc.css('a')[6].children.text
+		@post_one = html_doc.css('a')[7].children.text
 		@post_three = html_doc.css('a')[22].children.text
 		@post_two = html_doc.css('a')[12].children.text
 		@projects = Project.all.limit(3).order("created_at desc")
