9ef6880803b61b73533387cca0043fc8cff9e41c
espa77 <ethansteiner@gmail.com>
2017-07-13 00:07:23 -0400

    changing blog links

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,9 +4,9 @@ class WelcomeController < ApplicationController
 		agent = Mechanize.new { |agent| agent.user_agent_alias = "Mac Safari" }
 		html = agent.get(url).body
 		html_doc = Nokogiri::HTML(html)
-		@post_one = html_doc.css('a')[3].children.text
+		@post_one = html_doc.css('a')[1].children.text
 		@post_three = html_doc.css('a')[16].children.text
-		@post_two = html_doc.css('a')[7].children.text
+		@post_two = html_doc.css('a')[9].children.text
 		@projects = Project.all.limit(3).order("created_at desc")
 	end
 end
